31cd4d056ff6e966182bf9bc011d1039eae98f53
Alex-M1 <68870636+Alex-M1@users.noreply.github.com>
2023-11-04 14:21:44 +0200

    Add installer

---
+++ install.ps1
@@ -0,0 +1,14 @@
+# Запускаем Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+
+# Массив с именами программ для установки
+
+$programs = @('googlechrome','firefox', 'winrar', 'webstorm', 'vscode', 'deezer', 'zoom', 'nodejs', 'volta', 'slack', 'discord', 'telegram', 'git')
+
+foreach ($program in $programs) {
+    choco install $program -y --force
+}
+
+# для запуска скриптов Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
